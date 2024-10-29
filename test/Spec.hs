96330fde6fb4075d92e15613a229aba007b95456
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-10-29 08:01:36 +0000

    open and close database to use it without bracket

--- test/Spec.hs
+++ test/Spec.hs
@@ -26,83 +26,111 @@ withTestDBCF cfs go =
   withSystemTempDirectory "rocksdb-tests-cf" $ \path ->
     withDBCF path conf (map (,conf) cfs) go
 
+withTestDB :: (MonadUnliftIO m) => (DB -> m a) -> m a
+withTestDB go =
+  withSystemTempDirectory "rocksdb-tests" $ \path ->
+    withDB path conf go
+
 main :: IO ()
-main = do
-  hspec $ around (withTestDBCF ["one", "two", "tree"]) $ do
-    describe "Database" $ do
-      it "puts and gets an item" $ \db -> do
-        put db "aaa" "zzz"
-        get db "aaa" `shouldReturn` Just "zzz"
-      it "puts and gets from different type families" $ \db -> do
-        let two = head $ columnFamilies db
-        put db "aaa_key" "aaa_value"
-        get db "aaa_key" `shouldReturn` Just "aaa_value"
-        getCF db two "aaa_key" `shouldReturn` Nothing
-        putCF db two "two_key" "two_value"
-        getCF db two "two_key" `shouldReturn` Just "two_value"
-        get db "two_key" `shouldReturn` Nothing
-    describe "Multithreading" $ do
-      it "stores and retrieve items from multiple threads" $ \db -> do
-        let key i = C.pack $ printf "key_%04d" i
-            val i = C.pack $ printf "val_%04d" i
-            indices = [0 .. 9999] :: [Int]
-            keys = map key indices
-            vals = map val indices
-            kvs = zip keys vals
-        was <- mapM (\(k, v) -> async $ put db k v) kvs
-        mapM_ wait was
-        ras <- mapM (async . get db) keys
-        mapM wait ras `shouldReturn` map Just vals
-    describe "Iterators" $ do
-      it "retrieves entries using iterators" $ \db -> do
-        let key i = C.pack $ printf "key_%03d" i
-            val i = C.pack $ printf "val_%03d" i
-            indices = [0 .. 999] :: [Int]
-            keys = map key indices
-            vals = map val indices
-            kvs = zip keys vals
-        was <- mapM (\(k, v) -> async $ put db k v) kvs
-        mapM_ wait was
-        kvs' <- withIter db $ \itr -> do
-          let start = keys !! 500
-          iterSeek itr start
-          fmap catMaybes $ replicateM 500 $ do
-            mkv <- iterEntry itr
-            iterNext itr
-            return mkv
-        kvs' `shouldBe` drop 500 kvs
-      it "walks back and forth" $ \db -> do
-        withIter db $ \itr -> do
-          iterSeek itr "hello"
-          iterKey itr `shouldReturn` Nothing
-          iterValue itr `shouldReturn` Nothing
-          iterEntry itr `shouldReturn` Nothing
-        put db "a" "aaa"
-        put db "b" "bbb"
-        put db "c" "ccc"
-        withIter db $ \itr -> do
-          iterSeek itr "b"
-          iterKey itr `shouldReturn` Just "b"
-          iterValue itr `shouldReturn` Just "bbb"
+main = hspec $ do
+  describe "withDBCF" $
+    around (withTestDBCF ["one", "two", "tree"]) $ do
+      describe "Database" $ do
+        testPutGet
+        testColumnFamilies
+      testMultithreading
+      testIterators
+  describe "withDB (open/close)" $
+    around withTestDB $ do
+      describe "Database" $
+        testPutGet
+      testMultithreading
+      testIterators
+
+testPutGet :: SpecWith DB
+testPutGet =
+  it "puts and gets an item" $ \db -> do
+    put db "aaa" "zzz"
+    get db "aaa" `shouldReturn` Just "zzz"
+
+testColumnFamilies :: SpecWith DB
+testColumnFamilies =
+  it "puts and gets from different type families" $ \db -> do
+    let two = head $ columnFamilies db
+    put db "aaa_key" "aaa_value"
+    get db "aaa_key" `shouldReturn` Just "aaa_value"
+    getCF db two "aaa_key" `shouldReturn` Nothing
+    putCF db two "two_key" "two_value"
+    getCF db two "two_key" `shouldReturn` Just "two_value"
+    get db "two_key" `shouldReturn` Nothing
+
+testMultithreading :: SpecWith DB
+testMultithreading =
+  describe "Multithreading" $ do
+    it "stores and retrieve items from multiple threads" $ \db -> do
+      let key i = C.pack $ printf "key_%04d" i
+          val i = C.pack $ printf "val_%04d" i
+          indices = [0 .. 9999] :: [Int]
+          keys = map key indices
+          vals = map val indices
+          kvs = zip keys vals
+      was <- mapM (\(k, v) -> async $ put db k v) kvs
+      mapM_ wait was
+      ras <- mapM (async . get db) keys
+      mapM wait ras `shouldReturn` map Just vals
+
+testIterators :: SpecWith DB
+testIterators =
+  describe "Iterators" $ do
+    it "retrieves entries using iterators" $ \db -> do
+      let key i = C.pack $ printf "key_%03d" i
+          val i = C.pack $ printf "val_%03d" i
+          indices = [0 .. 999] :: [Int]
+          keys = map key indices
+          vals = map val indices
+          kvs = zip keys vals
+      was <- mapM (\(k, v) -> async $ put db k v) kvs
+      mapM_ wait was
+      kvs' <- withIter db $ \itr -> do
+        let start = keys !! 500
+        iterSeek itr start
+        fmap catMaybes $ replicateM 500 $ do
+          mkv <- iterEntry itr
           iterNext itr
-          iterKey itr `shouldReturn` Just "c"
-          iterValue itr `shouldReturn` Just "ccc"
-          iterNext itr -- After the last entry
-          iterKey itr `shouldReturn` Nothing
-          iterValue itr `shouldReturn` Nothing
-          iterPrev itr -- It can't come back
-          iterKey itr `shouldReturn` Nothing
-          iterValue itr `shouldReturn` Nothing
-        withIter db $ \itr -> do
-          iterSeek itr "b"
-          iterKey itr `shouldReturn` Just "b"
-          iterValue itr `shouldReturn` Just "bbb"
-          iterPrev itr
-          iterKey itr `shouldReturn` Just "a"
-          iterValue itr `shouldReturn` Just "aaa"
-          iterPrev itr -- Invalid before lowest key
-          iterKey itr `shouldReturn` Nothing
-          iterValue itr `shouldReturn` Nothing
-          iterNext itr -- But it remembers previous position
-          iterKey itr `shouldReturn` Just "b"
-          iterValue itr `shouldReturn` Just "bbb"
+          return mkv
+      kvs' `shouldBe` drop 500 kvs
+    it "walks back and forth" $ \db -> do
+      withIter db $ \itr -> do
+        iterSeek itr "hello"
+        iterKey itr `shouldReturn` Nothing
+        iterValue itr `shouldReturn` Nothing
+        iterEntry itr `shouldReturn` Nothing
+      put db "a" "aaa"
+      put db "b" "bbb"
+      put db "c" "ccc"
+      withIter db $ \itr -> do
+        iterSeek itr "b"
+        iterKey itr `shouldReturn` Just "b"
+        iterValue itr `shouldReturn` Just "bbb"
+        iterNext itr
+        iterKey itr `shouldReturn` Just "c"
+        iterValue itr `shouldReturn` Just "ccc"
+        iterNext itr -- After the last entry
+        iterKey itr `shouldReturn` Nothing
+        iterValue itr `shouldReturn` Nothing
+        iterPrev itr -- It can't come back
+        iterKey itr `shouldReturn` Nothing
+        iterValue itr `shouldReturn` Nothing
+      withIter db $ \itr -> do
+        iterSeek itr "b"
+        iterKey itr `shouldReturn` Just "b"
+        iterValue itr `shouldReturn` Just "bbb"
+        iterPrev itr
+        iterKey itr `shouldReturn` Just "a"
+        iterValue itr `shouldReturn` Just "aaa"
+        iterPrev itr -- Invalid before lowest key
+        iterKey itr `shouldReturn` Nothing
+        iterValue itr `shouldReturn` Nothing
+        iterNext itr -- But it remembers previous position
+        iterKey itr `shouldReturn` Just "b"
+        iterValue itr `shouldReturn` Just "bbb"
